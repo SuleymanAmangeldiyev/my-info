7eacfdfd921f60bf35840eb494842d8c085d2df8
Suleyman <suleyman20071998@gmail.com>
2023-05-13 00:20:43 +0300

    final

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+40731446177">+40 731-446-177</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:suleyman.webdev@gmail.com">
+            suleyman.webdev@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/SuleymanAmangeldiyev">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/suleyman-amangeldiyev-sai/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://google.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "PanCake",
+    description:
+      "On this project we got a great experience to work as a team. So we know that it's important to work as a team to build a new kinds of project as team. Also it is different to work by ourself than as a team. The aim of this project is to learn as a team with defferent countries' people.",
+    image: "/images/pancake.jpg",
+    tags: ["javascript", "html", "css"],
+    source: "https://google.com",
+    visit: "https://team-project-pancake.netlify.app/",
+    id: 0,
+  },
+  {
+    title:"SAI",
+    description:
+      "With the SAI system can do unlimited things... For example: 1: we can use any kind of information from anywhere, where the system is working. including: money, passports, Id cards, documents, phone's data and much more..",
+    image: "/images/sai.jpg",
+    tags: ["React", "JavaScript", "..."],
+    source: "https://google.com",
+    visit: "",
+    id: 1,
+  },
+  {
+    title: "HooBank",
+    description:
+      " As a front-end project, it is likely that HooBank is responsible for the user interface and user experience of the application. This could include designing and implementing the screens for sending and receiving money, as well as integrating with any necessary APIs or backend services to facilitate these transactions....",
+    image: "/images/handbook.jpg",
+    tags: ["React"],
+    source: "https://google.com",
+    visit: "https://my-hand-book.netlify.app/",
+    id: 2,
+  },
+  {
+    title: "CatFlix",
+    description:
+      "On this project we got a great experience to work as a team. So we know that it's important to work as a team to build a new kinds of project as team. Also it is different to work by ourself than as a team. The aim of this project is to learn as a team with defferent countries' people.",
+    image: "/images/catflix.jpg",
+    tags: ["React", "CSS", "HTML"],
+    source: "https://google.com",
+    visit: "https://team-project-catflix.netlify.app/",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2022, text: 'Started my journey', },
+  { year: 2022, text: 'Worked as a self empoyed', },
+  { year: 2022, text: 'Founded SAI project', },
+  { year: 2023, text: 'Shared my projects with the world', },
+  { year: 2023, text: 'Started my own platform', },
+];
